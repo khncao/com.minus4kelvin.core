42fc85e1f30fc4cc51e7afd16089bfafc83c927b
Kelvin Cao <khncao@gmail.com>
2021-11-20 05:46:59 -0600

    Fix build error with undefined editor code

--- Runtime/UI/UIThemeSO.cs
+++ Runtime/UI/UIThemeSO.cs
@@ -24,6 +24,7 @@ public class UIThemeSO : ScriptableObject {
     public Color accent1;
     public Color tint1;
 
+#if UNITY_EDITOR
     [ContextMenu("Apply Theme")]
     void Apply() {
         // var objs = FindObjectsOfType<UIObject>();
@@ -67,4 +68,6 @@ public class UIThemeSO : ScriptableObject {
         
         AssetDatabase.SaveAssets();
     }
+#endif
+
 }
